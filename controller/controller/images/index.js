bd5dee6ce2db03cf3b3e96c6e6327b52e45b11f3
Subki <tajudinsubki@gmail.com>
2020-10-21 15:24:20 +0700

    Add files via upload

---
+++ controller/controller/images/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const asyncAwait = require('express-async-await');
+const app = require('../..');
+const auth = require('../../middleware/auth')('api');
+const mod = new (require('../../model/images'))(app.pool);
+const { validateImage } = require('./validation');
+const util = require('../../util');
+
+router.get("/", auth, async (req, res)=> {
+    let [result, err] = await mod.find(req.query);
+    if(err) return res.status(422).json({status:1, ...result});
+
+    res.json(result);
+});
+
+router.get("/:id", auth, async (req, res)=> {
+    let [result, err] = await mod.get(req.params.id);
+    if(err) return res.status(422).json({status:1, ...result});
+
+    res.json(result);
+});
+
+
+router.post("/", auth, validateImage, async (req, res)=>{
+    let data = req.body;
+    if(data.file){
+        util.writeBase64(data.file, {createThumb:false, path:'./private/images/', ext:'.jpg'}, async function (resu) {
+            if (resu.status > 0) {
+                return res.status(422).json({status: 1, ...resu});
+            } else {
+                data.path_image = resu.data.file_name;
+                data.crtby = req.userInfo.id;
+                const [result, err] = await mod.insert(data);
+                if (err) return res.status(422).json({status: 1, ...result});
+                return res.json(result);
+            }
+        })
+    }else{
+        return res.status(422).json({status:1, errors:[{key:"general", msg:"Logo harus terlampir!"}]});
+    }
+});
+
+router.put("/:id", auth, async (req, res) => {
+    const data = req.body;
+    let param = {
+        fields: {...data, updby: req.userInfo.id},
+        where: {'id': req.params.id}
+    }
+    const [result, err] = await mod.update(param);
+    res.status(err?422:200).json(result);
+});
+
+router.delete("/:id", auth, async (req, res)=> {
+    const [result, err] = await mod.delete({key:'id', value:req.params.id})
+    if(err) return res.status(422).json({status:1, ...result});
+    res.json(result);
+});
+
+module.exports = asyncAwait(router);
